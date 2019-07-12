470ee22ace1c45cffd4ebcdc26c5d07107e6c81f
Selvaraj Chinnasamy <selvaraj.chinnasamy@softwaysolutions.com>
2019-07-12 17:48:33 +0530

    first commit

---
+++ electron/dist/main.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+var fs = require("fs");
+var win;
+electron_1.app.on('ready', createWindow);
+electron_1.app.on('activate', function () {
+    if (win === null) {
+        createWindow();
+    }
+});
+function createWindow() {
+    win = new electron_1.BrowserWindow({ width: 800, height: 600 });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "/../../dist/angular-electron/index.html"),
+        protocol: 'file:',
+        slashes: true,
+    }));
+    win.webContents.openDevTools();
+    win.on('closed', function () {
+        win = null;
+    });
+}
+// to access node js API's
+electron_1.ipcMain.on('getFiles', function (event, arg) {
+    var files = fs.readdirSync(__dirname);
+    win.webContents.send('getFilesResponse', files);
+});
+//# sourceMappingURL=main.js.map
